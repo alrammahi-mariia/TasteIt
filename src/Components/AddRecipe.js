a630450eaf9f516ed6131a25cb0b0589c5fcdb63
Your Name <you@example.com>
2022-05-10 14:00:46 +0300

    added addrecipe form view

--- src/Components/AddRecipe.js
+++ src/Components/AddRecipe.js
@@ -2,8 +2,26 @@ import React from "react";
 
 const AddRecipe = () => {
   return (
-    <div>
-      <p>Add recipe form will be here</p>
+    <div className="form-container">
+      <h2>Add new recipe</h2>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+        <label htmlFor="author">Author</label>
+        <input type="text" name="author" id="author" />
+        <label htmlFor="description">Description</label>
+        <textarea name="description" id="description" />
+        <label htmlFor="image">Image</label>
+        <input type="url" name="image" id="image" />
+        <label htmlFor="ingredients">Ingredients</label>
+        <label htmlFor="quantity">Quantity</label>
+        <input type="text" name="quantity" />
+        <label htmlFor="ingredient">Ingredient</label>
+        <input type="text" name="ingredient" />
+        <label htmlFor="instructions">Instructions</label>
+        <textarea name="instructions" id="instructions" />
+        <input type="submit" value="Post recipe" />
+      </form>
     </div>
   );
 };
